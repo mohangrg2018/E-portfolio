2ad67b25375dcfe7763067ad86285323b690dda8
mohangrg <mohangrg2030@gmail.com>
2023-07-04 08:56:54 +0545

    added emailjs in contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,11 +1,37 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_zll1ijc",
+        "template_a34wz5s",
+        form.current,
+        "b6jmytI3uYLIoxinJ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div
-      className="lg:max-w-6xl sm:max-w-2xl max-w-md mx-auto mt-36 p-8 bg-gray-700 rounded-lg"
+      className="lg:max-w-6xl sm:max-w-2xl max-w-md mx-auto mt-36 p-8 bg-[#0B0D0C] rounded-lg"
       id="contact"
     >
       <div className="sm:flex items-center">
@@ -39,37 +65,42 @@ const Contact = () => {
             <p>+977 9806722179</p>
           </Link>
         </div>
-        <div className="sm:w-[50%] mt-7">
+        <form ref={form} onSubmit={sendEmail} className="sm:w-[50%] mt-7">
           <input
             type="text"
+            name="name"
             placeholder="Enter Name*"
             className="w-full px-4 py-4 bg-transparent outline-none border-2 border-gray-300 mb-7"
           />
           <div className="w-full sm:flex items-center gap-2 mb-7">
             <input
               type="email"
+              name="email"
               placeholder="Enter Email*"
               className="px-3 py-4 w-full bg-transparent outline-none border-2 border-gray-300"
             />
             <input
               type="tel"
+              name="telephone"
               placeholder="Enter Number*"
               className="px-4 py-4 mt-7 sm:mt-0 w-full bg-transparent outline-none border-2 border-gray-300"
             />
           </div>
           <textarea
+            name="message"
             className="w-full px-4 py-4 mb-7 bg-transparent outline-none border-2 border-gray-300"
             placeholder="Enter Message*"
             cols="30"
             rows="4"
           ></textarea>
           <button
-            type="button"
+            onClick={sendEmail}
+            type="submit"
             className="px-8 py-3 border-2 border-primary text-primary uppercase hover:text-white hover:bg-primary"
           >
             Submit message
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
--- components/Services.jsx
+++ components/Services.jsx
@@ -2,7 +2,7 @@ import ProgressBar from "@ramonak/react-progress-bar";
 
 const Services = () => {
   return (
-    <div className="mt-10 flex items-center gap-6 text-white sm:p-20 p-10 bg-gray-700 rounded-md gap-50">
+    <div className="mt-10 flex items-center gap-6 text-white sm:p-20 p-10 bg-[#0B0D0C] rounded-md gap-50">
       <div className="w-[50%] flex flex-col gap-10 text-sm">
         <div>
           <p>HTML</p>
